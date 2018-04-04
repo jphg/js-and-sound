acdde49f652c592b21281efdab30678c2d2c9ada
jphg <jphg@kea.dk>
2018-04-04 19:42:04 +0200

    made ready for 100% volume function

--- javascript.js
+++ javascript.js
@@ -3,6 +3,8 @@ let mySound=document.querySelector("#mySound");
 let playBtn = document.querySelector("#play-btn");
 let pauseBtn = document.querySelector("#pause-btn");
 let stopBtn = document.querySelector("#stop-btn");
+let volume15Btn = document.querySelector("#volume15-btn");
+let volume100Btn = document.querySelector("#volume100-btn");
 
 
 
@@ -10,6 +12,8 @@ playBtn.addEventListener("click", playSound);
 pauseBtn.addEventListener("click", pauseSound);
 stopBtn.addEventListener("click", stopSound);
 
+volume15Btn.addEventListener("click", volume15);
+// addEventListener to the volume100Btn here
 
 function playSound(){
     mySound.play();
@@ -21,3 +25,11 @@ function stopSound(){
     mySound.pause();
     mySound.currentTime=0;
 }
+function volume15(){
+    mySound.volume=0.15;
+}
+
+//make the volume100 function here
+
+
+
